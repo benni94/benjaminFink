27d6f0279f4a79b33c59b3ce0e061573f79f4a6a
bfink <beni_@live.at>
2024-11-01 14:14:13 +0100

    update title

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export const App: React.FC = () => {
       }}
     >
       <h1>Hello my name is Benjamin</h1>
-      <h2>Check out my awesome website</h2>
+      <h2>Check out my awesome website:</h2>
       <span style={{ paddingTop: 50 }}>
         <img style={{ borderRadius: 20 }} src={homeLogo} alt="App Logo" />
       </span>
