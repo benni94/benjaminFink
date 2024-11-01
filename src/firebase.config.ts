bb012e6fd1a8cd715a89cf3a7f9f856542cdabe5
bfink <beni_@live.at>
2024-11-01 14:02:05 +0100

    add firebase config to deploy

---
+++ src/firebase.config.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAu9XRsE_oVBgEtUc51B8msnvqQEOB4TIs",
+    authDomain: "benjaminfinkme.firebaseapp.com",
+    projectId: "benjaminfinkme",
+    storageBucket: "benjaminfinkme.firebasestorage.app",
+    messagingSenderId: "890953803608",
+    appId: "1:890953803608:web:0cb09651aada282105e72d",
+    measurementId: "G-57WX3MCKX0"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig); 
